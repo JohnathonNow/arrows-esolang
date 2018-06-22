a4a33524cb67ca322bcebf039344818c4ec6e9d1
John Westhoff <johnjwesthoff@gmail.com>
2018-06-22 10:23:04 -0400

    Changed enum to enum-compat

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 
 setup(
     name = 'arrows_esolang',
-    version = '0.0.7',
+    version = '0.0.8',
     author = 'John Westhoff',
     author_email = 'johnjwesthoff@gmail.com',
     description = ('An esoteric language that uses drawings of arrows as the source code'),
     url = 'https://github.com/JohnathonNow/arrows-esolang',
     packages=['arrows_esolang'],
     scripts=['bin/arrows', 'bin/arrowsc'],
-    install_requires=['pillow', 'enum'],
+    install_requires=['pillow', 'enum-compat'],
     include_package_data=True,
 )
