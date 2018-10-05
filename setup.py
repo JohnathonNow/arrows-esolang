d8e84cb63a16c824866cf8c9ecd015170b62adbe
Mike E <39808659+AndroidKitKat@users.noreply.github.com>
2018-10-04 21:41:36 -0400

    Update setup.py
    Fixed typo

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version = '0.0.8',
     author = 'John Westhoff',
     author_email = 'johnjwesthoff@gmail.com',
-    description = ('An esoteric language that uses drawings of arrows as the source code'),
+    description = ('An esoteric language that uses drawings of arrows as the source code.'),
     url = 'https://github.com/JohnathonNow/arrows-esolang',
     packages=['arrows_esolang'],
     scripts=['bin/arrows', 'bin/arrowsc'],
