9ae8895229954612e721b22ffc94e2c5c97af2b0
John Westhoff <johnjwesthoff@gmail.com>
2019-10-31 23:43:24 -0400

    Version change

--- setup.py
+++ setup.py
@@ -3,18 +3,14 @@ from setuptools import setup
 
 setup(
     name='arrows_esolang',
-    version='0.0.8.1',
+    version='1.0.0',
     author='John Westhoff',
     author_email='johnjwesthoff@gmail.com',
     description=('An esoteric language that uses drawings of arrows.'),
     url='https://github.com/JohnathonNow/arrows-esolang',
     packages=['arrows_esolang'],
     test_suite='tests',
-    scripts=[
-        'bin/arrows',
-        'bin/arrowsc'],
-    install_requires=[
-        'pillow',
-        'enum-compat'],
+    scripts=['bin/arrows', 'bin/arrowsc'],
+    install_requires=['pillow', 'enum-compat'],
     include_package_data=True,
 )
