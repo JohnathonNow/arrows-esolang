15983f40cc1b9f41100f3f600040297dc0589525
John Westhoff <johnjwesthoff@gmail.com>
2019-10-31 23:50:57 -0400

    Added long description

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
-from setuptools import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='arrows_esolang',
-    version='1.0.1',
+    version='1.0.2',
     author='John Westhoff',
     author_email='johnjwesthoff@gmail.com',
     description=('An esoteric language that uses drawings of arrows.'),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/JohnathonNow/arrows-esolang',
-    packages=['arrows_esolang'],
+    packages=setuptools.find_packages(),
     test_suite='tests',
     scripts=['bin/arrows', 'bin/arrowsc'],
     install_requires=['pillow', 'enum-compat'],
